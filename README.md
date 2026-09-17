bf822bc0ea448509911ebfd0e3272a063f1519ec
sannchu <sannchu@users.noreply.github.com>
2026-09-17 19:32:10 +0200

    Initial commit for react-clase

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-clase
+
+Proyecto de clase desarrollado con React y Vite.
+
+## Comandos
+
+```bash
+npm install
+npm run dev
+```
+
+Para validar el proyecto:
+
+```bash
+npm run lint
+npm run build
+```
---
+++ src/componentes/zonaCliente/registro/LoginRegistro.jsx
@@ -0,0 +1,42 @@
+import "./LoginRegistro.css";
+
+
+
+
+
+
+function LoginRegistro() {
+    
+
+    const cajasTexto = ["Nombre", "Email", "Contraseña", "Repetir Contraseña"];
+
+    
+    return <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <img src="/imagenes/logo-pccomponentes-registro.svg" alt="logo" width="200" height="100" />
+                </div>
+            </div>
+
+            <div className="row">
+
+                <div className="col-md-6">
+                    <img src="/imagenes/Captura de pantalla 2026-09-17 190515.png" alt="Captura" />
+                </div>
+                <div className="col-md-6">
+                    <h2>Crear cuenta</h2>
+
+                    {cajasTexto.map((elemento, posicion) => (
+                        <div key={posicion} className="form-floating mb-3">
+                            <input type="text" className="form-control" id={`floatingInput${posicion}`} placeholder={elemento}/>
+                            <label htmlFor={`floatingInput${posicion}`}>{elemento}</label>
+                        </div>
+                    ))}
+
+                    <button type="submit" className="btn btn-primary">Crear cuenta</button>
+                </div>
+            </div>  
+        </div>
+}
+
+export default LoginRegistro;
