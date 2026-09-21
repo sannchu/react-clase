48c4d57e23d042f8572c79ba0c768e4f8cae04d4
sannchu <sannchu@users.noreply.github.com>
2026-09-21 18:22:52 +0200

    Actualizar formulario y estados de registro

--- pc-componentes/src/App.jsx
+++ pc-componentes/src/App.jsx
@@ -20,6 +20,12 @@ function App() {
     const [ password, setPassword ] = useState(''); // valor inicial del password es cadena vacia
     const [ repassword, setRepassword ] = useState(''); // valor inicial del repassword es cadena vacia
 
+    const camposFormulario = [
+      { label: "Nombre", type:"text" }, 
+      { label:"Email", type:"email" }, 
+      { label:"Password", type:"password" }, 
+      { label:"Repassword", type:"password" }
+    ];
   //el hook useState te devuelve un ARRAY con dos elementos:
   //- en la primera posicion esta el valor actual de la variable
   //- en la segunda posicion te devuelve una FUNCION QUE PERMITE ACTUALIZAR/MODIFICAR su valor desde codigo
@@ -33,37 +39,81 @@ function App() {
 
   // }
 
-  function changeNombre(ev){
-    console.log('evento change del input-nombre, el valor actual de la caja de texto es....', ev.target.value);
-    setNombreUsuario(ev.target.value); //llamo a la funcion modificadora de esta variable del state y asigno nuevo valor....
-    console.log('nombre de usuario vale ahora: ', nombreUsuario);
+  // function changeNombre(ev){
+  //   console.log('evento change del input-nombre, el valor actual de la caja de texto es....', ev.target.value);
+  //   setNombreUsuario(ev.target.value); //llamo a la funcion modificadora de esta variable del state y asigno nuevo valor....
+  //   console.log('nombre de usuario vale ahora: ', nombreUsuario);
+  // }
+
+  // function changeEmail(ev){
+  //   console.log('evento change del input-email, el valor actual de la caja de texto es....', ev.target.value);
+  //   setEmail(ev.target.value);
+  //   console.log('email vale ahora: ', email);
+  // }
+
+  // function changePassword(ev){
+  //   console.log('evento change del input-password, el valor actual de la caja de texto es....', ev.target.value);
+  //   setPassword(ev.target.value);
+  //   console.log('password vale ahora: ', password);
+  // }
+
+  // function changeRepassword(ev){
+  //   console.log('evento change del input-repassword, el valor actual de la caja de texto es....', ev.target.value);
+  //   setRepassword(ev.target.value);
+  //   console.log('repassword vale ahora: ', repassword);
+  // }
+
+  function changeInputs(ev){
+    console.log('evento change del input: ',ev, '...el valor actual de la caja de texto es....', ev.target.value);
+
+    switch (
+ev.target.id
+) {
+      case 'inputNombre': setNombreUsuario(ev.target.value);break;
+      case 'inputEmail': setEmail(ev.target.value);break;
+      case 'inputPassword': setPassword(ev.target.value);break;
+      case 'inputRepassword': setRepassword(ev.target.value);break;
+    }
+
   }
 
-  function changeEmail(ev){
-    console.log('evento change del input-email, el valor actual de la caja de texto es....', ev.target.value);
-    setEmail(ev.target.value);
-    console.log('email vale ahora: ', email);
+  //boton CREAR CUENTA SOLO SE HABILITA si las variables del state cumplen:
+  //- todas rellenadas
+  //- email tiene formato de email, lo suyo seria comprobar tb dominios
+  //- la password entre 6 y 64 caracteres, tiene q tener MAYS, MINS, digitos y caracteres especiaels
+  //- la password y repassword son iguales
+  //- RECAPTCHA codigo antibot ok
+  function clickCrearCuenta(){
+    console.log('datos a mandar al server....', { nombreUsuario, email, password, repassword });
   }
 
-  function changePassword(ev){
-    console.log('evento change del input-password, el valor actual de la caja de texto es....', ev.target.value);
-    setPassword(ev.target.value);
-    console.log('password vale ahora: ', password);
-  }
-
-  function changeRepassword(ev){
-    console.log('evento change del input-repassword, el valor actual de la caja de texto es....', ev.target.value);
-    setRepassword(ev.target.value);
-    console.log('repassword vale ahora: ', repassword);
-  }
-  
   return (
     <>
       <h1>Vamos a comprobar lo que es el STATE de un componente de REACT</h1>
       {/* <p>Contador vale: { valorStateContador[0] }</p> <p>Contador vale: { contador }</p>
       <button className="btn btn-primary" onClick={ clickBoton }> INCREMENTA EL CONTADOR</button> */}
 
-      <div className="m-4">
+      {
+        
+camposFormulario.map
+( campo => (
+                                        <div className="m-4" key={campo.label}>
+                                          <label className="form-label" htmlFor={`input${campo.label}`}>{campo.label}:</label>
+                                          <input type={campo.type} 
+                                                  id={`input${campo.label}`} 
+                                                  className="form-control" 
+                                                  placeholder={campo.label + "*"} 
+                                                  onChange={ changeInputs }/>
+                                        </div>
+                                      )
+        )
+      }
+
+      <div className="m-4 d-flex flex-row justify-content-center">
+        <button type="button" className="btn btn-primary" onClick={ clickCrearCuenta }>Crear cuenta</button>
+      </div>
+
+      {/* <div className="m-4">
         <label className="form-label" htmlFor="inputNombre">Nombre Usuario: { nombreUsuario }</label>
         <input type="text" 
                 id="inputNombre" 
@@ -97,21 +147,11 @@ function App() {
                 className="form-control" 
                 placeholder="Repetir Contraseña*" 
                 onChange={ changeRepassword }/>
-      </div>
-
-      <div className="m-4">
-        <label className="form-label" htmlFor="inputNombre">Nombre Usuario: { nombreUsuario }</label>
-        <input type="text" 
-                id="inputNombre" 
-                className="form-control" 
-                placeholder="Escribe tu nombre de usuario" 
-                onChange={ changeNombre }/>
-      </div>
-
-
+      </div> */}
 
     </>
   )
 }
 
-export default App
+export default App 
+
