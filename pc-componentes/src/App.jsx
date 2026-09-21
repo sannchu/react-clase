20cd91d8fc52abd8c75fb1495d681f0b51af3a37
sannchu <sannchu@users.noreply.github.com>
2026-09-21 17:06:29 +0200

    Copiar ejercicio de useState en App

--- pc-componentes/src/App.jsx
+++ pc-componentes/src/App.jsx
@@ -1,120 +1,40 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //let contador = 0;
+  //metemos variable contador en el STATE DEL COMPONENTE, usando hook useState()
+  // useState() devuelve un array con dos elementos: el valor actual del estado y una función para actualizarlo
+
+  const valorStateContador = useState(0); // valor inicial del contador es 10
+
+  console.log('valor devuelto por el hook useState() al meter variable contador en el STATE DEL COMPONENTE: ', valorStateContador);
+  //el hook useState te devuelve un ARRAY con dos elementos:
+  //- en la primera posicion esta el valor actual de la variable
+  //- en la segunda posicion te devuelve una FUNCION QUE PERMITE ACTUALIZAR/MODIFICAR su valor desde codigo
+  console.log(' - primera posicion del array: VALOR ACTUAL de la variable...', valorStateContador[0]);
+  console.log(' - segunda posicion del array: FUNCION MODIFICADORA de la variable....', valorStateContador[1].toString());
+  let contador = valorStateContador[0]; //valor actual del contador
+  let setContador = valorStateContador[1]; //funcion que permite modificar el valor del contador
+
+  function clickBoton(ev){
+      console.log('evento0 interceptado en funcion click del boton...', ev);
+      // actualizamos el valor del contador llamando a la funcion que nos devuelve el hook useState()
+      setContador(contador + 1);
+      //console.log('contador vale ahora: ', valorStateContador[0]);
+      console.log('contador vale ahora: ', contador);
+  }
+      //console.log('contador vale ahora: ', contador);
+
+
+  // }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <h1>Vamos a comprobar lo que es el STATE de un componente de REACT</h1>
+      {/* <p>Contador vale: {valorStateContador[0]}</p> */}
+      <p>Contador vale: {contador}</p>
+      <button className="btn btn-primary" onClick={ clickBoton }> INCREMENTA EL CONTADOR</button>
     </>
   )
 }
