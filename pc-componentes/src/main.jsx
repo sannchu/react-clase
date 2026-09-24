b0b9de6391b7c05db9c116412d50bb7b5c87f7ad
sannchu <sannchu@users.noreply.github.com>
2026-09-24 18:06:48 +0200

    Configurar ruta de login y registro

--- pc-componentes/src/main.jsx
+++ pc-componentes/src/main.jsx
@@ -1,11 +1,40 @@
+import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router'
+
 //import App from './App.jsx'
-import LoginRegistro from './componentes/zonaCliente/registro/LoginRegistro.jsx';
+import LoginRegistro from './componentes/zonaCliente/registro/LoginRegistro.jsx'
+
+
+//#region ------- configuracion modulo de enrutamiento react:  paquete react-router ------
+
+//1ºpaso: instalar el paquete react-router-dom: npm install react-router --save
+
+//2º paso: configurar las diferentes rutas a interceptar por el modulo de enrutamiento y configurar que
+//componente se va a renderizar. Para hacerlo se usa la funcion: createBrowerRouter(...)
+//esta funcion recibe como parametros un array de objetos js q deben implementar la clase Route de react-router
+//cada objeto Route tiene al menos estas dos propiedades:
+// - 'path': ruta a interceptar en el navegador
+// - 'element' o 'Component': componente a renderizar cuando se intercepta la ruta
+
+  const routerObjects=createBrowserRouter(
+    [
+      { path: '/Cliente/LoginRegistro', element: <LoginRegistro /> },
+    ] //<---- array de objetos Route de react-router
+  )
+
+//3º paso: comoo compnente principal de la aplicacion se debe renderizar el componente de react-router: <RouterProvider..></RouterProvider..>
+// este componente recibe como propiedades el objeto de rutas creado en el paso 2 y lo activa...
+
+//#endregion
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LoginRegistro />
+    <RouterProvider router={routerObjects} />
   </StrictMode>,
-)
+) 
+
